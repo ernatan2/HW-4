4f37fa45a9ff472e43d0dca1056cc34583691655
NATHAN-PC\ernat <ernatan2@gmail.com>
2022-05-17 18:34:46 +0300

    Finished function 2

--- HW 4/Main.c
+++ HW 4/Main.c
@@ -36,6 +36,8 @@ Student* readFromBinFile(const char* fileName);
 
 int main()
 {
+	
+
 	//Part A
 	int* coursesPerStudent = NULL;
 	int numberOfStudents = 0;
@@ -60,20 +62,62 @@ int main()
 
 void countStudentsAndCourses(const char* fileName, int** coursesPerStudent, int* numberOfStudents)
 {
-	
+	FILE* studentList;
+	int counterOfStudents = 0, i = 1; //Creating intiger for counting
+	*numberOfStudents = 0;
+	studentList = fopen(fileName, "r"); //Set the file path
+	if (!studentList) {
+		printf("Error opening file\n");
+		exit(1);
+	}
+	char* tempBuffer = NULL; //Creating a string for the function to count the pipes
+	while (!feof(studentList)) {
+		if (i == 1) {
+			tempBuffer = (char*)malloc(i * sizeof(char*));
+		}
+		else {
+			tempBuffer = realloc(tempBuffer, i * sizeof(char)); // Allocating memory for the string
+		}
+		if (!tempBuffer) {
+			printf("Memory allocation faild\n");
+			exit(1);
+		}
+		tempBuffer[i-1] = fgetc(studentList); 
+		i++;
+		if ((tempBuffer[i-2] == '\n') || (feof(studentList))) {
+			if (!counterOfStudents) {
+				coursesPerStudent = (int*)malloc(sizeof(int*));
+			}
+			else {
+				coursesPerStudent = realloc(coursesPerStudent, (counterOfStudents + 1) * sizeof(*coursesPerStudent));
+			}
+			if (!coursesPerStudent) {
+				printf("Memory allocation faild\n");
+				exit(1);
+			}
+			const char* lineBuffer = tempBuffer; //Creating a const string for the function
+			*(coursesPerStudent + counterOfStudents) = countPipes(lineBuffer, i); //Set the number of the pipes in the pointer
+			counterOfStudents++;
+			*numberOfStudents = counterOfStudents; 
+			free(tempBuffer);
+			i = 1;
+		}
+		
+	}
+	fclose(studentList);
 }
 
 int countPipes(const char* lineBuffer, int maxCount)
 {
-	int i = 0, p_count = 0;
-	if (lineBuffer = NULL) return -1;
-	if (maxCount <= 0) return 0;
-	while (*(lineBuffer + i) != '\0' && i < maxCount)
+	int p_count = 0, counter = 0;; //Creating the intigers 
+	if (!lineBuffer) return -1; //Checking if lineBuffer points to Null
+	if (maxCount <= 0) return 0; //Checking if maxCount less than zero
+	while (*(lineBuffer + counter) != '\0' || p_count == maxCount) //Counting the pipes in the string
 	{
-		if (*(lineBuffer + i) == '|') p_count++;
-		i++;
+		if (*(lineBuffer + counter) == '|') p_count++;
+		counter++;
 	}
-	return p_count;
+	return p_count; //Returning the pipes count
 }
 
 char*** makeStudentArrayFromFile(const char* fileName, int** coursesPerStudent, int* numberOfStudents)
@@ -90,7 +134,7 @@ void printStudentArray(const char* const* const* students, const int* coursesPer
 {
 	for (int i = 0; i < numberOfStudents; i++)
 	{
-		printf("name: %s\n*********\n", students[i][0]);
+		printf("name: %s\n*********\n", students[i][0]); 
 		for (int j = 1; j <= 2 * coursesPerStudent[i]; j += 2)
 		{
 			printf("course: %s\n", students[i][j]);
